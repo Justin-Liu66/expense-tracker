e5dcca21e6a3f97b53bfad7803bea5dababd7fff
Justin <s101407028@gmail.com>
2022-11-03 12:00:39 +0800

    add mongoose

--- app.js
+++ app.js
@@ -2,7 +2,26 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGODB_URI)
+// 非正式環境時, 使用 dotenv
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+// 連線MongoDB
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true, useUnifiedTopology: true
+})
+//取得連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
 
 app.get('/', (req, res) => {
   res.send('hello new project')
