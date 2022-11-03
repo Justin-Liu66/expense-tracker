31e4639c822c67445604525ddc077e5c7665ebdf
Justin <s101407028@gmail.com>
2022-11-03 11:29:48 +0800

    add dotenv

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.MONGODB_URI)
 
 app.get('/', (req, res) => {
   res.send('hello new project')
