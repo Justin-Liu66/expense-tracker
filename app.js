7b215537fc29bbf5383c97252370998c45a754f4
Justin <s101407028@gmail.com>
2022-11-04 21:03:12 +0800

    add delete record function

--- app.js
+++ app.js
@@ -84,6 +84,14 @@ app.post('/records/:id/edit', (req, res) => {
     .catch(err => console.log(err))
 })
 
+app.post('/records/:id/delete', (req, res) => {
+  const id = req. params.id
+  return Record.findById(id)
+    .then(record => record.remove())
+    .then(() => res.redirect('/'))
+    .catch(err => console.log(err))
+})
+
 
 app.listen(3000, () => {
   console.log('App is running on http://localhost:3000')
