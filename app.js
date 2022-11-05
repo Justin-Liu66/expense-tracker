ce77e49b6b39f661e2abee474b5e34dd186211e8
Justin <s101407028@gmail.com>
2022-11-05 11:41:36 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,31 +1,12 @@
 const express = require('express')
-const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 
 const routes = require('./routes')
+require('./config/mongoose')
 
 const app = express()
-
-// 非正式環境時, 使用 dotenv
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
-// 連線MongoDB
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true, useUnifiedTopology: true
-})
-// 取得連線狀態
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
 
 // app中新增hbs引擎
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
