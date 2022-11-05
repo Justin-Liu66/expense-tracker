4cf3987d6bfeb556e3447247d62b33286b3ff143
Justin <s101407028@gmail.com>
2022-11-05 14:20:20 +0800

    add bootstrap & modify main.hbs

--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -5,7 +5,6 @@ db.once('open', () => {
 
   Array.from({ length: 10 }, (_, i) => {
     Record.create({
-      id: i + 1,
       name: `name-${i + 1}`,
       date: '2022.11.04',
       amount: '100'
