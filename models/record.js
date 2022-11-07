75bf1df45216d0ad94580fb0edb2081159304b0b
Justin <s101407028@gmail.com>
2022-11-07 11:23:04 +0800

    add Category Model

--- models/record.js
+++ models/record.js
@@ -13,13 +13,15 @@ const recordSchema = new Schema({
     type: Number,
     required: true
   },
-  category: {
-    type: String,
-    required: true
-  },
   userId: {
     type: Schema.Types.ObjectId,
     ref: 'User',
+    index: true,
+    required: true
+  },
+  categoryId: {
+    type: Number,
+    ref: 'Category',
     index: true,
     required: true
   }
