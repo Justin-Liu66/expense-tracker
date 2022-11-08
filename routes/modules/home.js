5c58d99db1c1634867dbbe029aae21f075ab2425
Justin <s101407028@gmail.com>
2022-11-08 20:58:26 +0800

    load icon into pages (version 1)

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -3,6 +3,23 @@ const router = express.Router()
 
 const Record = require('../../models/record')
 
+// icon 測試中
+const categoryNum = {
+  1: '家居物業',
+  2: '交通出行',
+  3: '休閒娛樂',
+  4: '餐飲食品',
+  5: '其他'
+}
+
+const CATEGORY = {
+  家居物業: 'fa-solid fa-house',
+  交通出行: 'fa-solid fa-van-shuttle',
+  休閒娛樂: 'fa-solid fa-face-grin-beam',
+  餐飲食品: 'fa-solid fa-utensils',
+  其他: 'fa-solid fa-pen'
+}
+
 router.get('/', (req, res) => {
   const userId = req.user._id
   Record.find({ userId })
@@ -16,6 +33,13 @@ router.get('/', (req, res) => {
         totalAmount += record.amount
         // 將日期轉換為yyyy/mm/dd
         record.date = record.date.toLocaleDateString()
+        // icon測試中!!!!!!!!
+        const number = record.categoryId
+        const categoryChinese = categoryNum[number]
+        const icon = CATEGORY[categoryChinese]
+        record.icon = icon 
+
+
       })
       res.render('index', { records, totalAmount })
     }
